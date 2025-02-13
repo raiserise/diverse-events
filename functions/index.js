a3d36f16a019a0ce1f4514236c74bbcb8ecbf1c4
Ryan <91196378+raiserise@users.noreply.github.com>
2025-02-13 14:30:33 +0800

    Add new toast, fix security vulnerabilities

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const express = require("express");
 // const dotenv = require("dotenv");
@@ -20,10 +20,10 @@ app.use("/", userRoutes);
 
 // // Local development: Run Express normally if not in Firebase Functions
 // if (process.env.NODE_ENV !== "prod") {
-// const port = process.env.SERVER_PORT || 8080;
-// app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}`);
-// });
+const port = process.env.SERVER_PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 // }
 
 // Firebase Function (V2) - Expose Express API
@@ -31,6 +31,6 @@ exports.api = onRequest(app);
 
 // Example Firebase Function (Standalone)
 exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
+  logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
--- src/App.js
+++ src/App.js
@@ -9,56 +9,61 @@ import UsersPage from "./pages/users/Users";
 import Signup from "./pages/signup/Signup";
 import ProtectedRoute from "./components/ProtectedRoute";
 import ProfilePage from "./pages/profile/Profile";
+import "react-toastify/dist/ReactToastify.css";
+import CustomToast from "./components/CustomToast";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Layout title={"Dashboard"}>
-                <DashboardPage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/events"
-          element={
-            <ProtectedRoute>
-              <Layout title={"Events"}>
-                <EventsPage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/users"
-          element={
-            <ProtectedRoute>
-              <Layout title={"User Management"}>
-                <UsersPage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Layout title={"Events"}>
-                <ProfilePage />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        {/* Add more routes if needed */}
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Layout title={"Dashboard"}>
+                  <DashboardPage />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/events"
+            element={
+              <ProtectedRoute>
+                <Layout title={"Events"}>
+                  <EventsPage />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute>
+                <Layout title={"User Management"}>
+                  <UsersPage />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Layout title={"Events"}>
+                  <ProfilePage />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* Add more routes if needed */}
+        </Routes>
+      </Router>
+      <CustomToast />
+    </div>
   );
 };
 
