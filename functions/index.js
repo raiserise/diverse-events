9ccff8bcb39d2b2b5072091e308bf92561dfbbb2
Ryan <91196378+raiserise@users.noreply.github.com>
2025-04-20 18:59:45 +0800

    Update pipeline to generate coverage report

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,6 @@
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const { onRequest } = require("firebase-functions/v2/https");
 const express = require("express");
-// const dotenv = require("dotenv");
 const cors = require("cors");
-
-// Load environment variables
-// dotenv.config();
 
 // Initialize Express app
 const app = express();
@@ -26,17 +21,17 @@ app.use("/", notificationRoutes);
 
 // // Local development: Run Express normally if not in Firebase Functions
 // if (process.env.NODE_ENV !== "prod") {
-const port = process.env.SERVER_PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// const port = process.env.SERVER_PORT || 8080;
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
 // }
 
 // Firebase Function (V2) - Expose Express API
 exports.api = onRequest(app);
 
-// Example Firebase Function (Standalone)
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// // Example Firebase Function (Standalone)
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
